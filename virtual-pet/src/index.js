9f447a6f7ca3ff3825e8079926e7f8aba129b6a9
xiaoshuai <qixiaoshuai@outlook.com>
2018-08-26 23:07:13 +0800

    add es6 class

--- virtual-pet/src/index.js
+++ virtual-pet/src/index.js
@@ -1,34 +1,31 @@
 import 'phaser';
 
-var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
+class MainScene {
+    preload() {
+        this.load.image('logo', 'assets/logo.png');
+    }
+
+    create() {
+        var logo = this.add.image(400, 150, 'logo');
+
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: 'Power2',
+            yoyo: true,
+            loop: -1
+        });
+
     }
 };
 
+var config = {
+    type: Phaser.AUTO,
+    parent: 'virtual-pet',
+    width: 800,
+    height: 600,
+    scene: [MainScene]
+};
+
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('logo', 'assets/logo.png');
-}
-
-function create ()
-{
-    var logo = this.add.image(400, 150, 'logo');
-
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
-
-}
