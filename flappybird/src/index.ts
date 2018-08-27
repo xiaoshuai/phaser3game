e4a5c0d20c443104fe9df6855bbcc40d6f1c376e
wanjia <qixiaoshuai@outlook.com>
2018-08-28 00:11:46 +0800

    finish flappybird

--- flappybird/src/index.ts
+++ flappybird/src/index.ts
@@ -1,16 +1,42 @@
 import "phaser";
 import { BootScene } from "./bootScene";
+import { MainMenuScene } from "./mainMenuScene";
 import { GameScene } from "./gameScene";
 
 const config: GameConfig = {
-    backgroundColor: "#000000",
+    title: "Flappy Bird",
+    url: "https://github.com/digitsensitive/phaser3-typescript",
+    version: "1.0",
+    width: 405,
     height: 600,
-    parent: "virtual-pet",
-    scene: [BootScene, GameScene],
+    zoom: 1,
     type: Phaser.AUTO,
-    width: 800,
+    parent: "game",
+    scene: [BootScene, MainMenuScene, GameScene],
+    input: {
+        keyboard: true,
+        mouse: false,
+        touch: false,
+        gamepad: false
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    backgroundColor: "#98d687",
+    // pixelArt: true,
+    // antialias: false
 };
 
+export class Game extends Phaser.Game {
+    constructor(config: GameConfig) {
+        super(config);
+    }
+}
+
 window.onload = () => {
-    const game = new Phaser.Game(config);
+    var game = new Game(config);
 };
